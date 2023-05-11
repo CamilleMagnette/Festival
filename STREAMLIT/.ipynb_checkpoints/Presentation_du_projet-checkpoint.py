1bed465020c96ef62b3fadabb116cbaea1067d7b
camille magnette <camille.magnette@icloud.com>
2023-05-11 21:57:58 +0200

    first sync

---
+++ STREAMLIT/.ipynb_checkpoints/Presentation_du_projet-checkpoint.py
@@ -0,0 +1,142 @@
+import streamlit as st
+import pandas as pd
+from PIL import Image
+
+
+
+st.set_page_config(
+    page_title="HACKATHON",
+    layout="wide",
+    page_icon=":🎼:")
+
+# st.sidebar.markdown("# BLABLABLA")
+
+st.markdown("# FESTIVAL LES PETITS POUCETS 🎼")
+st.subheader('Les 5 et 6 juillet 2023')
+
+
+# IMPORTER LE DATAFRAME
+#link = "blablabla"
+#df = pd.read_csv(link)
+
+
+# CREER DES TAB 
+tab1, tab2, tab3, tab4 = st.tabs(['BESOIN CLIENT', 'ENJEUX & PROBLEMATIQUE', 'EQUIPE VASCALYTICS', 'NOTRE PROPOSITION PAR ETAPE'])
+
+
+# TRAVAILLER SUR LES TAB 
+
+with tab1 : 
+
+    # DEMANDE CLIENT 
+
+    st.subheader('PROPOSITION DE PROGRAMMATION MUSICALE')
+
+    st.write("Jeune label en quête de notoriété vous souhaiter promouvoir vos artistes via l'organisation d'un festival musical.") 
+
+
+    # PHOTO CONCERT EXTERIEUR    
+    image_coeur = Image.open('images/image_festival_coeur.jpg')
+    
+    image_container = st.container()
+    with image_container:
+        st.image(image_coeur, width=900)
+
+
+with tab2 : 
+    
+    # ENJEUX & PROBLEMATIQUE
+
+    st.subheader('VOS ENJEUX')
+
+    st.write("1) 💰 Vous rémunérer") 
+    st.write("2) 🔈 Gagner en visibilité avec des artistes reconnus") 
+    st.write("3) 🔦 Faire découvrir de nouveaux talents") 
+    st.write("4) 🔎 Trouver un équilibre entre tête d'affiche et nouveaux talents") 
+
+    st.subheader('NOTRE PROBLEMATIQUE')
+    
+    st.write("Quelle programmation d'artistes mettre en place pour la réussite de cet evenement ?") 
+
+
+with tab3 : 
+    
+    # EQUIPE VASCALYTICS
+
+    st.subheader('UNE EQUIPE A VOTRE ECOUTE')
+    
+    st.write("Nous mettons à votre disposition 6 jeunes consultants pendant 2 jours ") 
+
+    # PHOTO CONCERT EXTERIEUR    
+    vincent = Image.open('images/Vincent.png')
+    chrysanthe = Image.open('images/Chrysanthe.jpeg')
+    camille = Image.open('images/Camille.jpg')
+    stephanie = Image.open('images/Stephanie.jpeg')
+    anthony = Image.open('images/Anthony.jpeg')
+    amine = Image.open('images/Amine.jpeg')
+
+    image_container = st.container()
+
+    # création de la grille horizontale
+    col1, col2, col3, col4, col5, col6 = st.columns(6)
+    
+    # Centrage horizontal des boutons
+    #st.markdown("""<style>.centered-image {display: flex;justify-content: center;align-items: center;}</style>""", unsafe_allow_html=True)
+    
+    # Images en ligne
+    with col1 :
+        
+        #with image_container:
+            st.image(vincent, width=100)
+            st.write("Vincent Messika")           
+    with col2 :
+        #with image_container:
+            st.image(chrysanthe, width=100)
+            st.write("Chrysanthe Delrieu")      
+    with col3 :
+        #with image_container:
+            st.image(camille, width=100)
+            st.write("Camille Magnette")         
+    with col4 :
+        #with image_container:
+            st.image(stephanie, width=100)
+            st.write("Stephanie Bourganel")   
+    with col5 :
+        #with image_container:
+            st.image(anthony, width=100)
+            st.write("Anthony Iervasi")   
+    with col6 :
+        #with image_container:
+            st.image(amine, width=100)
+            st.write("Amine Aissami") 
+
+    #st.markdown("""<style>.stimage { display: flex; justify-content: center; align-items: center;}</style>""", unsafe_allow_html=True)
+
+with tab4 : 
+    
+    # NOTRE PROPOSITION PAR ETAPE
+
+    # création de la grille horizontale
+    col1, col2, col3 = st.columns(3)
+
+    with col1 :
+        lunettes = Image.open('images/image_lunette.jpg')
+        st.image(lunettes, width=400)
+        
+    with col2 :
+
+        st.subheader('Sommaire')
+
+        st.write("1) Nos sources de données") 
+        st.write("2) Etat des lieux du marché musical actuel") 
+        st.write("3) Performance et positionnement des artistes managés") 
+        st.write("4) Recommandation d'artistes à pousser pour le festival") 
+        st.write("5) Recommandation de programmation sur deux jours") 
+        
+    with col3 :
+
+        st.write(" ") 
+
+    
+    
+# METHODOLOGIE : mapping à présenter 
---
+++ STREAMLIT/pages/.ipynb_checkpoints/02 Marche_musical_actuel-checkpoint.py
@@ -0,0 +1,94 @@
+import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+import plotly.express as px
+
+
+st.markdown("# LE MARCHE DU STREAM ACTUEL 🎧")
+st.subheader('Les talents du moment...et jeunes talents à venir')
+
+#st.sidebar.markdown("# Les talents du moment 🎼")
+
+
+# Importer le dataset : Janvier 2017 à dec 2021
+df_top_200 = pd.read_csv("datasets/top_200.csv", sep = ",")
+# Afficher le dataframe
+# st.write(df_top_200)
+
+
+# CREER DES TAB 
+tab1, tab2, tab3, tab4 = st.tabs(['TOP 10 ARTISTES FRANCE', 'TOP 10 MORCEAUX', 'STATISTIQUES STREAM', 'blabla'])
+
+
+
+# TRAVAILLER SUR LES TAB 
+
+with tab1 : 
+
+    st.subheader('Artistes les plus streamés sur Spotify en 2021')
+    st.write("Taille des bulles : nombre total de stream en 2021")
+    
+    # Filtrer les données pour la région France
+    top_200_france = df_top_200[df_top_200['region'] == 'France']
+
+    # Regrouper les données par artiste et calculer le nombre total de streams pour chaque artiste
+    artist_streams = top_200_france.groupby('artist')['streams'].sum()
+
+    # Ajouter une colonne pour le trend de chaque artiste (MOVE_UP, SAME_POSITION, ou MOVE_DOWN)
+    artist_trend = top_200_france.groupby('artist')['trend'].max()
+    artist_streams_trend = pd.concat([artist_streams, artist_trend], axis=1)
+
+    # Trier les artistes en fonction de leur position dans le top 200
+    top_artists = artist_streams_trend.sort_values('streams', ascending=False)[:10]
+
+    # Créer le graphique à bulles
+    fig, ax = plt.subplots(figsize=(12,8))
+    for artist, row in top_artists.iterrows():
+        ax.scatter(artist, row['streams'], s=row['streams']/100000, alpha=0.5)
+        ax.annotate(artist, (artist, row['streams']), ha='center')
+
+    #pd.options.display.float_format = '{:.0f}'.format
+
+    
+    # Configurer le graphique
+    ax.set_title('Artistes français', fontsize=16)
+    ax.set_xlabel('Artistes', fontsize=12)
+    ax.set_ylabel('Nombre total de streams (Milliards)', fontsize=12)
+    ax.grid(True)
+
+    # Afficher le graphique
+    st.pyplot(fig.figure)
+    
+with tab2 : 
+    
+    st.subheader('Morceaux les plus streamés en 2021')
+    
+    # Groupby par titre pour avoir la somme des streams de chaque titre
+    top_by_title = df_top_200.groupby(['title', 'artist']).sum()
+
+    # Filtrer les 10 titres ayant la valeur de stream maximale
+    top10 = top_by_title.sort_values('streams', ascending=False).head(10)
+
+    # Créer une liste contenant le titre et l'artiste pour chaque titre dans top10
+    titles_artists = [f"{title} - {artist}" for title, artist in top10.index]
+
+    # Créer le graphique
+    fig, ax = plt.subplots(figsize=(10, 6))
+
+    # Ajouter les barres pour les streams
+    ax.bar(titles_artists, top10['streams'], color='blue')
+
+    # Configurer le graphique
+    ax.set_title('Top 10 titres', fontsize=16)
+    ax.set_xlabel('Titre - Artiste', fontsize=12)
+    ax.set_ylabel('Stream (en millions)', fontsize=12)
+
+    plt.xticks(rotation=90)
+
+    # Afficher le graphique
+    st.pyplot(fig.figure)
+
+    
+with tab4 : 
+    
+    st.subheader('blabla')
